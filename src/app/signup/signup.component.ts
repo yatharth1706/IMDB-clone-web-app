6a8ad41b315965d9680b9113c4cf447682a778e0
unknown <yatharthverma070@gmail.com>
2019-07-04 11:20:28 +0530

    added formGroup,formBuilder,formControl,validators

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,30 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  myForm: FormGroup;
 
-  constructor() { }
+  constructor(public fb: FormBuilder) {
+    this.myForm=this.fb.group({
+      firstName: ['',[Validators.required]],
+      lastName: ['',[Validators.required]],
+      email: ['',[Validators.required]],
+      password: ['',[Validators.required,Validators.minLength(8)]],
+      confirmPassword: ['',[Validators.required]]
+    })
+   }
 
   ngOnInit() {
   }
 
-i: any = 0;
-txt: string = 'Lorem ipsum typing effect!'; /* The text */
-speed: any = 50; /* The speed/duration of the effect in milliseconds */
-
-typeWriter() {
-  if (this.i < this.txt.length) {
-    document.getElementById("typewriter").innerHTML += this.txt.charAt(this.i);
-    this.i++;
-    setTimeout(this.typeWriter, this.speed);
+  onSubmit(myForm){
+    console.log(myForm);
   }
-}
-  
-onclik(){
-  this.typeWriter();
-}
+
 }
