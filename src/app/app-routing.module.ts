1b6e8b0e656958142259e307770cbbe155a7570d
unknown <yatharthverma070@gmail.com>
2019-07-04 11:57:13 +0530

    added routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DisplayMoviesComponent } from './display-movies/display-movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login' ,component: LoginComponent},
+  {path: 'signup' , component: SignupComponent},
+  {path: 'movies', component: DisplayMoviesComponent},
+  {path: '**', redirectTo: 'login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
