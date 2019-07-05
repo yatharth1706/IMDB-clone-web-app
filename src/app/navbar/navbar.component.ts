563181f86acd837dac01a266574d76bf55116c96
unknown <yatharthverma070@gmail.com>
2019-07-05 12:23:47 +0530

    updated code

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
+  
+  searchMovie(query){
+    // url="http://omdbapi.com/?s=" + query + "&apikey=thewdb"
+    this.http.get("http://omdbapi.com/?s=avengers&apikey=thewdb").subscribe((data)=>{
+      console.log(data);
+    })
 
+  }
 }
