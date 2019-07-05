2a68c800d7c5fbaff8afd416322422db2ac223e9
unknown <yatharthverma070@gmail.com>
2019-07-05 15:32:18 +0530

    added pictures along with movies title

--- src/app/display-movies/display-movies.component.ts
+++ src/app/display-movies/display-movies.component.ts
@@ -15,13 +15,19 @@ export class DisplayMoviesComponent implements OnInit {
 
   url:any;
   movies: any;
+  posters: any[]=[];
   searchMovie(query){
     this.url="http://omdbapi.com/?s=" + query + "&apikey=thewdb"
     
     this.http.get(this.url).subscribe((data)=>{
-      
+      this.posters=[];
       this.movies=data;
-      console.log(this.movies['Search']);
+      console.log(this.movies['Search'][0]['Poster']);
+      for(let source in this.movies['Search']){
+        this.posters.push(this.movies['Search'][source]['Poster']);
+      }
+      
+      console.log(this.posters);
     })
 
   }
