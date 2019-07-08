dbe14bb959f533f7e2ea014a587ec2ac6b32f684
CodingNinja1706 <32243289+CodingNinja1706@users.noreply.github.com>
2019-07-08 14:55:22 +0530

    Delete auth.service.ts

--- src/app/auth.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthService {
-  apikey: string = "b703930dc226b4b1b4c1383288ce260d";
-  constructor() { }
-}
