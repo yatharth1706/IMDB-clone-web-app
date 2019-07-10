0062b7bd6409fae54646881b0d02d749d0fde5c5
unknown <yatharthverma070@gmail.com>
2019-07-10 20:10:42 +0530

    added sort & year functionality in search

--- src/app/discover/discover.component.ts
+++ src/app/discover/discover.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../auth.service'
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.component.html',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscoverComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+    this.auth.discover(this.year,this.genres,this.sortt).subscribe((data)=>{
+      console.log(data);
+      this.results=data['results'];
+    })
   }
 
+  year: number=2019;
+  sortt: string="popularity.desc";
+  genres: string="28"
+  results: any[]=[];
+
+  fn(){
+    console.log(this.year);
+    console.log(this.sortt);
+    console.log(this.genres);
+    this.auth.discover(this.year,this.genres,this.sortt).subscribe((data)=>{
+      console.log(data);
+      this.results=data['results'];
+    })
+  }
 }
