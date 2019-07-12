d8916dec3779816aa41988916b85334c084150d3
unknown <yatharthverma070@gmail.com>
2019-07-12 15:13:03 +0530

    added new links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { SignupComponent } from './signup/signup.component';
 import { DisplayMoviesComponent } from './display-movies/display-movies.component';
 import { TvShowComponent } from './tv-show/tv-show.component';
 import { DiscoverComponent } from './discover/discover.component';
+import { ViewinfoComponent } from './viewinfo/viewinfo.component';
 
 const routes: Routes = [
   {path: 'login' ,component: LoginComponent},
@@ -12,6 +13,8 @@ const routes: Routes = [
   {path: 'movies', component: DisplayMoviesComponent},
   {path: 'tvshows' ,component: TvShowComponent},
   {path: 'discover', component: DiscoverComponent},
+  {path: 'discover/:view/:id', component: ViewinfoComponent},
+  {path: 'movies/:view/:id', component: ViewinfoComponent},
   {path: '**', redirectTo: 'login'}
 ];
 
