0ce95c813b89c4825155bf7cb08462175b772326
unknown <yatharthverma070@gmail.com>
2019-07-12 15:11:42 +0530

    designed view component for movies components

---
+++ src/app/viewinfo/viewinfo.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService } from '../auth.service';
+import { ActivatedRoute } from '@angular/router';
+@Component({
+  selector: 'app-viewinfo',
+  templateUrl: './viewinfo.component.html',
+  styleUrls: ['./viewinfo.component.css']
+})
+export class ViewinfoComponent implements OnInit {
+  id:string;
+  results: any;
+  image: string;
+  constructor(private auth:AuthService,public activatedRoute: ActivatedRoute) {
+    let Id=this.activatedRoute.snapshot.paramMap.get("id");
+    this.id=Id;
+    
+   }
+   cast:any[]=[];
+   profile: any[]=[];
+  ngOnInit() {
+    this.auth.showInfo(this.id).subscribe((data)=>{
+      console.log(data);
+      this.results=data;
+      this.image="https://image.tmdb.org/t/p/original/" + this.results['backdrop_path'];  
+      this.cast=this.results['credits']['cast'];
+      console.log(this.cast);
+      for(let i=0;i<5;i++){
+        this.profile.push(this.cast[i]);
+      }
+      console.log(this.profile);
+    })
+    
+  }
+
+  showInfo(){
+
+    
+  }
+}
