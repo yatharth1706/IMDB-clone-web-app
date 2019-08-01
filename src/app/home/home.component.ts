64232c8c5c69734adb728d545cfc2e2d4955b05f
unknown <yatharthverma070@gmail.com>
2019-08-01 08:47:45 +0530

    changed home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,26 +7,12 @@ import {AuthService} from '../auth.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  results: any[]=[];
+  
   constructor(private auth:AuthService) { }
  
   ngOnInit() {
-    this.nowPlaying();
+    
   }
-  arrayRange: any[]=[];
-  imagesArray: any[]=[];
-  nowPlaying(){
-    this.auth.nowPlaying().subscribe((data)=>{
-      console.log(data);
-      this.results=data['results'];
-      console.log(this.results);
-      for(var i=0;i<this.results.length;i++){
-        this.arrayRange.push(i);
-        this.imagesArray.push("http://image.tmdb.org/t/p/original" + this.results[i]['poster_path']);
-      }
-      console.log(this.imagesArray);
-      console.log(this.arrayRange);
-    })
-  }
+ 
 
 }
